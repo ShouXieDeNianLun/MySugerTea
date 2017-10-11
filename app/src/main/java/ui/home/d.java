ccbee70156bdf1118bab8ebebf3b4f4de9f87435
ShouXieDeNianLun <wyj1073957356@163.com>
2017-10-11 15:37:12 +0800

    初次上传

---
+++ app/src/main/java/ui/home/d.java
@@ -0,0 +1,8 @@
+package ui.home;
+
+/**
+ * Created by acer-pc on 2017/10/10.
+ */
+
+public class d {
+}
---
+++ app/src/main/java/ui/login/ChooseLoginActivity.java
@@ -0,0 +1,23 @@
+package ui.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.wyj.cn.mysugertea.R;
+
+/**
+ * Created by acer-pc on 2017/10/6.
+ */
+
+public class ChooseLoginActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_choose_login);
+
+    }
+}
